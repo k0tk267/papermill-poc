7aca116cdb9d148fc28b5808b8fdfa5c948690e3
k0tk267 <k0tk2670@gmail.com>
2022-11-25 14:50:31 +0900

    feat: add run.py

---
+++ run.py
@@ -0,0 +1,15 @@
+import papermill as pm
+from dotenv import load_dotenv
+import os
+from datetime import datetime as dt
+
+load_dotenv()
+PROJECT_PATH=os.environ["PROHECT_PATH"]
+
+timestamp = dt.now().strftime('%Y-%m-%d-%Hh-%Mm')
+
+pm.execute_notebook(
+    f"{PROJECT_PATH}/notebooks/input/input.ipynb",
+    f"{PROJECT_PATH}/notebooks/output/output_{timestamp}.ipynb",
+    parameters=dict(alpha=5, delta=7)
+)
